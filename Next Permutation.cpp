d2718450fa469519c688dba16e20120b37e08ae0
fuwutu <fuwutu@qq.com>
2013-06-14 00:30:03 +0800

    Update Next Permutation.cpp

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -3,24 +3,23 @@ class Solution
 public:
     void nextPermutation(vector<int> &num)
     {
-        size_t n = num.size();
-        
-        for (size_t i = n - 1; i > 0; --i)
+        size_t i = num.size() - 1;
+        for (; i > 0; --i)
         {
             if (num[i] > num[i-1])
             {
-                for (size_t j = n - 1; j >= i; --j)
+                for (size_t j = num.size() - 1; j >= i; --j)
                 {
                     if (num[j] > num[i-1])
                     {
                         swap(num[i-1], num[j]);
-                        reverse(num.begin() + i, num.end());
-                        return;
+                        break;
                     }
                 }
+                break;
             }
         }
              
-        reverse(num.begin(), num.end());
+        reverse(num.begin() + i, num.end());
     }
 };
