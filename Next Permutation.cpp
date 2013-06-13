2ceb3a8fa27107a455fba8e995c6219f25a34dbc
fuwutu <fuwutu@qq.com>
2013-06-13 19:54:49 +0800

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &num)
+    {
+        size_t n = num.size();
+        if (n > 1)
+        {
+            for (size_t i = n - 1; i > 0; --i)
+            {
+                if (num[i] > num[i-1])
+                {
+                    for (size_t j = n - 1; j >= i; --j)
+                    {
+                        if (num[j] > num[i-1])
+                        {
+                            swap(num[i-1], num[j]);
+                            reverse(num.begin() + i, num.end());
+                            return;
+                        }
+                    }
+                }
+            }
+             
+            reverse(num.begin(), num.end());
+        }
+    }
+};
