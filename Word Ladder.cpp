f028469009257e9c856824e6d75d21b6020b9890
fuwutu <fuwutu@qq.com>
2013-06-14 21:31:12 +0800

    Update Word Ladder.cpp

--- Word Ladder.cpp
+++ Word Ladder.cpp
@@ -3,41 +3,26 @@ class Solution
 public:
     int ladderLength(string start, string end, unordered_set<string> &dict)
     {
-        vector<string> reach({start});
-        vector<string> temp;
+        vector<string> reach({start}), temp;
         int step = 1;
         while (!reach.empty() && find(reach.begin(), reach.end(), end) == reach.end())
         {
             step += 1;
-            
             for (auto it = reach.begin(); it != reach.end(); ++it)
             {
                 string& s = *it;
-                
                 for (size_t i = 0; i < s.length(); ++i)
                 {
                     char back = s[i];
-                    for (char c = 'a'; c <= 'z'; ++c)
-                    {
-                        s[i] = c;
-                        if (c != back && dict.erase(s) == 1)
-                        {
+                    for (s[i] = 'a'; s[i] <= 'z'; ++s[i])
+                        if (s[i] != back && dict.erase(s) == 1)
                             temp.push_back(s);
-                        }
-                    }
                     s[i] = back;
                 }
             }
-            
             reach.swap(temp);
             temp.clear();
         }
-        
-        if (find(reach.begin(), reach.end(), end) != reach.end())
-        {
-            return step;
-        }
-        
-        return 0;
+        return find(reach.begin(), reach.end(), end) != reach.end() ? step : 0;
     }
 };
