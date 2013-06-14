76421b75a4813c206a5e018747e3513cc28b96a3
fuwutu <fuwutu@qq.com>
2013-06-14 21:53:25 +0800

    Update Word Ladder.cpp

--- Word Ladder.cpp
+++ Word Ladder.cpp
@@ -3,26 +3,36 @@ class Solution
 public:
     int ladderLength(string start, string end, unordered_set<string> &dict)
     {
-        vector<string> reach({start}), temp;
-        int step = 1;
-        while (!reach.empty() && find(reach.begin(), reach.end(), end) == reach.end())
+        if (dict.count(start) && dict.count(end))
         {
-            step += 1;
-            for (auto it = reach.begin(); it != reach.end(); ++it)
+            if (start == end)
+                return 1;
+                
+            vector<string> reach({start}), temp;
+            int step = 1;
+            while (!reach.empty())
             {
-                string& s = *it;
-                for (size_t i = 0; i < s.length(); ++i)
+                step += 1;
+                for (auto it = reach.begin(); it != reach.end(); ++it)
                 {
-                    char back = s[i];
-                    for (s[i] = 'a'; s[i] <= 'z'; ++s[i])
-                        if (s[i] != back && dict.erase(s) == 1)
-                            temp.push_back(s);
-                    s[i] = back;
+                    string& s = *it;
+                    for (size_t i = 0; i < s.length(); ++i)
+                    {
+                        char back = s[i];
+                        for (s[i] = 'a'; s[i] <= 'z'; ++s[i])
+                            if (s[i] != back && dict.erase(s) == 1)
+                            {
+                                if (s == end)
+                                    return step;
+                                temp.push_back(s);
+                            }
+                        s[i] = back;
+                    }
                 }
+                reach.swap(temp);
+                temp.clear();
             }
-            reach.swap(temp);
-            temp.clear();
         }
-        return find(reach.begin(), reach.end(), end) != reach.end() ? step : 0;
+        return 0;
     }
 };
