0646ab541c97d8261284dea9eb0e1433ac76ef8d
fuwutu <fuwutu@qq.com>
2013-06-16 18:05:31 +0900

    Update Search a 2D Matrix.cpp

--- Search a 2D Matrix.cpp
+++ Search a 2D Matrix.cpp
@@ -16,3 +16,33 @@ public:
         return false;
     }
 };
+
+class Solution2
+{
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target)
+    {
+        const int row = matrix.size();
+        const int column = matrix[0].size();
+        int left = 0;
+        int right = row * column;
+        while (left < right)
+        {
+            int middle = (left + right) / 2;
+            int value = matrix[middle / column][middle % column];
+            if (value == target)
+            {
+                return true;
+            }
+            if (value < target)
+            {
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle;
+            }
+        }
+        return false;
+    }
+};
