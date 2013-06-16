c75b3d86038c0e9fc0eccc616e2fdeb1958bd862
fuwutu <fuwutu@qq.com>
2013-06-17 00:13:27 +0900

    Update Search for a Range.cpp

--- Search for a Range.cpp
+++ Search for a Range.cpp
@@ -3,6 +3,30 @@ class Solution
 public:
     vector<int> searchRange(int A[], int n, int target)
     {
+        vector<int> v;
+        for (int i = 0; i < n; ++i)
+        {
+            v.push_back(A[i]);
+        }
+        auto lower = lower_bound(v.begin(), v.end(), target);
+        auto upper = upper_bound(v.begin(), v.end(), target);
+        
+        if (lower == v.end() || *lower != target)
+        {
+            return vector<int>({-1, -1});
+        }
+        else
+        {
+            return vector<int>({lower - v.begin(), upper - v.begin() - 1});
+        }
+    }
+};
+
+class Solution2
+{
+public:
+    vector<int> searchRange(int A[], int n, int target)
+    {
         int left = 0;
         int right = n;
         while (left < right)
