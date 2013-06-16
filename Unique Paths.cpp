04e8d24b557e6bce31a0da0355ffcac622cdc061
fuwutu <fuwutu@qq.com>
2013-06-16 16:24:56 +0900

    Update Unique Paths.cpp

--- Unique Paths.cpp
+++ Unique Paths.cpp
@@ -1,6 +1,23 @@
 class Solution
 {
 public:
+    int uniquePaths(int m, int n)
+    {
+        vector<int> path(m, 1);
+        for (size_t i = 1; i < n; ++i)
+        {
+            for (size_t j = 1; j < m; ++j)
+            {
+                path[j] += path[j - 1];
+            }
+        }
+        return path.back();
+    }
+};
+
+class Solution2
+{
+public:
     int GCD(int m, int n)
     {
         if (m < n)
