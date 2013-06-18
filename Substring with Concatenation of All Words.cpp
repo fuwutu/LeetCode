43ac42c5c27368bc6db8be1d564e0979cc033c25
fuwutu <fuwutu@qq.com>
2013-06-18 21:29:14 +0800

    Update Substring with Concatenation of All Words.cpp

--- Substring with Concatenation of All Words.cpp
+++ Substring with Concatenation of All Words.cpp
@@ -1,96 +1,81 @@
 class Solution 
 {
 public:
-    void OneWordIn(const string& s, unordered_map<string, int>& word_count, int& not_match)
-    {
-        auto it = word_count.find(s);
-        if (it != word_count.end())
-        {
-            it->second -= 1;
-            if (it->second == 0)
-            {
-                not_match -= 1;
-            }
-        }
-    }
-
-    void OneWordOut(const string& s, unordered_map<string, int>& word_count, int& not_match)
-    {
-        auto it = word_count.find(s);
-        if (it != word_count.end())
-        {
-            if (it->second == 0)
-            {
-                not_match += 1;
-            }
-            it->second += 1;
-        }
-    }
-    
     vector<int> findSubstring(string S, vector<string> &L)
     {
         const size_t count = L.size();
         const size_t len = L[0].length();
-        string word;
-
+        const size_t total_len = len * count;
+        
         vector<int> result;
-        if (S.length() >= len * L.size())
+        if (S.length() >= total_len)
         {
-            unordered_map<string, int> word_count;
-            for (size_t i = 0; i < count; ++i)
+            map<string, int> word2count;
+            for (auto s : L)
             {
-                string& s = L[i];
-                auto ret = word_count.insert(make_pair(s, 1));
-                if (!ret.second)
-                {
-                    ret.first->second += 1;
-                }
+                word2count[s] = word2count[s] + 1;
             }
-
-            int not_match = word_count.size();
+            
+            string word;
             for (size_t start = 0; start < len; ++start)
             {
-                if (start + len * count <= S.length())
+                if (start + total_len <= S.length())
                 {
-                    size_t b1 = start;
-                    size_t b2 = start;
-                    for (size_t i = 0; i < count; ++i)
+                    auto word2require = word2count;
+                    size_t not_enough = word2count.size();
+                    size_t num = 0;
+                    size_t b = start;
+                    while (true)
                     {
-                        word.assign(S, b2, len);
-                        OneWordIn(word, word_count, not_match);
-                        b2 += len;
-                    }
-
-                    if (not_match == 0)
-                    {
-                        result.push_back(b1);
-                    }
-
-                    while (b2 + len <= S.length())
-                    {
-                        word.assign(S, b1, len);
-                        OneWordOut(word, word_count, not_match);
-                        b1 += len;
-
-                        word.assign(S, b2, len);
-                        OneWordIn(word, word_count, not_match);
-                        b2 += len;
-
-                        if (not_match == 0)
+                        if (num == count)
                         {
-                            result.push_back(b1);
+                            word.assign(S, b - total_len, len);
+                            auto it = word2require.find(word); // i am sure it is not equal to word2require.end()
+                            if (it->second == 0)
+                            {
+                                not_enough += 1;
+                            }
+                            it->second += 1;
+                            num -= 1;
                         }
-                    }
 
-                    while (b1 + len <= S.length())
-                    {
-                        word.assign(S, b1, len);
-                        OneWordOut(word, word_count, not_match);
-                        b1 += len;
+                        word.assign(S, b, len);
+                        b += len;
+                        num += 1;
+
+                        auto it = word2require.find(word);
+                        if (it != word2require.end())
+                        {
+                            it->second -= 1;
+                            if (it->second == 0)
+                            {
+                                not_enough -= 1;
+                                if (not_enough == 0)
+                                {
+                                    result.push_back(b - total_len);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (b + total_len > S.length())
+                            {
+                                break;
+                            }
+                            
+                            if (num != 1)
+                            {
+                                word2require = word2count;
+                                not_enough = word2count.size();
+                            }
+                            num = 0;
+                        }
+
                     }
                 }
             }
         }
+        
         return result;
     }
 };
