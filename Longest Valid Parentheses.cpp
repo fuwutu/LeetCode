58d1e291312c2e262e07094133fbf27a68eee849
fuwutu <fuwutu@qq.com>
2013-06-20 21:12:00 +0800

    Update Longest Valid Parentheses.cpp

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -3,6 +3,52 @@ class Solution
 public:
     int longestValidParentheses(string s)
     {
+        int longest = 0;
+        vector<int> position_count;
+        
+        int left = 0;
+        int count = 0;
+        for (size_t i = 0; i < s.length(); ++i)
+        {
+            if (s[i] == '(')
+            {
+                ++left;
+                position_count.push_back(count);
+                count = 0;
+            }
+            else
+            {
+                --left;
+                if (left != -1)
+                {
+                    count += 1;
+                    count += position_count.back();
+                    if (count > longest)
+                    {
+                        longest = count;
+                    }
+                    position_count.pop_back();
+                }
+                else
+                {
+                    left = 0;
+                    count = 0;
+                    position_count.clear();
+                }
+            }
+        }
+        
+        return longest * 2;
+    }
+};
+
+//=============================================================================
+
+class Solution2
+{
+public:
+    int longestValidParentheses(string s)
+    {
         int length = 0;
         for (int i = 0; i < s.length(); ++i)
         {
