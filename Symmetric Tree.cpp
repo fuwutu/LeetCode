b77888077f4c91f332790829fec06b7d24e40be4
fuwutu <fuwutu@qq.com>
2013-06-23 14:21:29 +0900

    Create Symmetric Tree.cpp

---
+++ Symmetric Tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isSymmetric(TreeNode* a, TreeNode* b)
+    {
+        if (a != nullptr && b != nullptr)
+        {
+            return (a->val == b->val)
+                && isSymmetric(a->left, b->right)
+                && isSymmetric(a->right, b->left);
+        }
+        
+        return (a == nullptr && b == nullptr);
+    }
+    
+    bool isSymmetric(TreeNode* root)
+    {
+        return isSymmetric(root, root);
+    }
+};
