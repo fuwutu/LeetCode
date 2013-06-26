1aa174baeaab5ef07e1ee959f8fa2f20864d2e08
fuwutu <fuwutu@qq.com>
2013-06-26 23:38:34 +0800

    Update Pow(x, n).cpp

--- Pow(x, n).cpp
+++ Pow(x, n).cpp
@@ -3,6 +3,35 @@ class Solution
 public:
     double pow(double x, int n)
     {
+        if (n < 0)
+        {
+            x = 1.0 / x;
+        }
+        
+        double result = 1.0;
+        double power = x;
+        
+        while (n != 0)
+        {
+            if (n % 2 != 0)
+            {
+                result *= power;
+            }
+            n /= 2;
+            power = power * power;
+        }
+        
+        return result;
+    }
+};
+
+//======================================================================
+
+class Solution2
+{
+public:
+    double pow(double x, int n)
+    {
         if (n == 0)
         {
             return 1.0;
