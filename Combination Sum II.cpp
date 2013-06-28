899e92d2322216aef7c4bc36fb33a73c67eb2e29
fuwutu <fuwutu@qq.com>
2013-06-28 21:25:29 +0800

    Update Combination Sum II.cpp

--- Combination Sum II.cpp
+++ Combination Sum II.cpp
@@ -5,6 +5,47 @@ public:
     {
         sort(num.begin(), num.end());
         
+        set<vector<int>> result_set;
+        vector<vector<int>> combination({{}});
+        for (int n : num)
+        {
+            size_t count = combination.size();
+            for (size_t i = 0; i < count; ++i)
+            {
+                int sum = accumulate(combination[i].begin(), combination[i].end(), 0);
+                if (sum + n < target)
+                {
+                    combination.push_back(combination[i]);
+                    combination.back().push_back(n);
+                }
+                else if (sum + n == target)
+                {
+                    auto x = combination[i];
+                    x.push_back(n);
+                    result_set.insert(x);
+                }
+            }
+        }
+        
+        vector<vector<int>> result;
+        for (auto& x : result_set)
+        {
+            result.push_back(x);
+        }
+        
+        return result;
+    }
+};
+
+//============================================================
+
+class Solution2
+{
+public:
+    vector<vector<int>> combinationSum2(vector<int>& num, int target)
+    {
+        sort(num.begin(), num.end());
+        
         vector<vector<int>> result;
         vector<vector<int>> combination({{}});
         for (int n : num)
