5b2fead824e982eeb5f4cd6b893d38a2d1391372
fuwutu <fuwutu@qq.com>
2013-06-29 09:08:03 +0800

    Create Interleaving String.cpp

---
+++ Interleaving String.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    bool isInterleave(string s1, string s2, string s3)
+    {
+        if (s1.size() + s2.size() != s3.size())
+        {
+            return false;
+        }
+        
+        vector<bool> match(s1.size() + 1, false);
+        match[0] = true;
+        
+        for (size_t c3 = 1; c3 <= s3.size(); ++c3)
+        {
+            if (c3 <= s2.size())
+            {
+                for (size_t c1 = min(s1.size(), c3); c1 >= 1 ; --c1)
+                {
+                    match[c1] = (match[c1-1] && s1[c1-1] == s3[c3-1] || match[c1] && c1 < c3 && s2[c3-c1-1] == s3[c3-1]);
+                }
+                match[0] = (match[0] && s2[c3-1] == s3[c3-1]);
+            }
+            else
+            {
+                for (size_t c1 = min(s1.size(), c3); c1 >= c3 - s2.size() ; --c1)
+                {
+                    match[c1] = match[c1-1] && s1[c1-1] == s3[c3-1] || match[c1] && c1 < c3 && s2[c3-c1-1] == s3[c3-1];
+                }
+            }
+        }
+        
+        return match[s1.size()];
+    }
+};
